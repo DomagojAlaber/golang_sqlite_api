644d4c7d3f943e517d0e6a11af6d6421208711c5
Domagoj <a.domagoj@hotmail.com>
2025-03-12 18:46:29 +0100

    Added models folder and test folder for testing endpoints

--- models/event.go
+++ models/event.go
@@ -1,1 +1,23 @@
 package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events = []Event{}
+
+func (event *Event) Save() {
+	events = append(events, *event)
+	// TODO: add to db
+}
+
+func GetAllEvents() ([]Event, error) {
+	return events, nil
+}
