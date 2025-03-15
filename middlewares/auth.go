abcf3817bf2eab9b160870c2601314be34e8ba24
Domagoj <a.domagoj@hotmail.com>
2025-03-15 19:26:44 +0100

    Adding middleware

---
+++ middlewares/auth.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"example.com/golang_sqlite_api/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "invalid credentials"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	context.Next()
+}
