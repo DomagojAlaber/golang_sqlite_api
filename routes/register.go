d6b6c0635c46e458c27b0af090519b2c6fd97e41
Domagoj <a.domagoj@hotmail.com>
2025-03-16 20:55:58 +0100

    Registrations for event

--- routes/register.go
+++ routes/register.go
@@ -1,9 +1,37 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"example.com/golang_sqlite_api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
+	if err != nil {
+		fmt.Println("Error: ", err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not find event"})
+		return
+	}
+
+	err = event.Register(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not register user for event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "successfully registerd"})
 }
 
 func cancelRegistration(context *gin.Context) {
